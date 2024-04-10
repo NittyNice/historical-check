9fa8d2bd4a9ae121b92d9473da641210272b85f8
NittyNice <165763929+NittyNice@users.noreply.github.com>
2024-04-10 10:15:01 +0700

    .

--- app.py
+++ app.py
@@ -345,11 +345,11 @@ if uploaded_tdt is not None:
             lines+markers
             if not norm_select:
                 for column in options:
-                    fig.add_trace(go.Scatter(x=hist_data_short.index, y=hist_data_short[column], mode=plot_select, name=column))
+                    fig.add_trace(go.Scatter(x=hist_data_short.index, y=hist_data_short[column], mode=str(plot_select), name=column))
             else:
                 for column in options:
                     hist_data_short_norm = (hist_data_short - hist_data_short.min()) / (hist_data_short.max() - hist_data_short.min())
-                    fig.add_trace(go.Scatter(x=hist_data_short_norm.index, y=hist_data_short_norm[column], mode=plot_select, name=column))
+                    fig.add_trace(go.Scatter(x=hist_data_short_norm.index, y=hist_data_short_norm[column], mode=str(plot_select), name=column))
 
             # Update figure layout
             fig.update_layout(
